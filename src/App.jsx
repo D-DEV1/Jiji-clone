18d6a3b9ebcb942f38e3a4e80804f3ba20d570e3
mac <mac@macs-MacBook-Air.local>
2025-09-07 23:47:04 +0100

    Updated version

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Navbar from './Components/Navbar'
+import Hero from './Components/Hero'
+import Main from './Components/main'
+import Footer from './Components/Footer'
+
+const App = () => {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <Main />
+      <Footer />
+    </>
+  )
+}
+
+export default App
+
+
